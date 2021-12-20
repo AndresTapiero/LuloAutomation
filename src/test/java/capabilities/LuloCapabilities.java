240d00a66bb2ec6dc776c2756a97971458385f5b
AndresTapiero <andres.tapiero@grability.com>
2021-12-19 22:55:57 -0500

    add relative route to capabilities

--- src/test/java/capabilities/LuloCapabilities.java
+++ src/test/java/capabilities/LuloCapabilities.java
@@ -1,36 +1,48 @@
 package capabilities;
 
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import static utils.Resources.*;
 
 public class LuloCapabilities {
-    public  static URL url;
+    public static URL url;
     public static DesiredCapabilities capabilities;
-    public static AndroidDriver<MobileElement> driver;
+    public static AndroidDriver androidDriver;
+    public static IOSDriver iosDriver;
+    public static String SO = System.getProperty("os.name");
+    public static final String APK_LOCATION_W = System.getProperty(USER_DIR) + "\\src\\test\\resources\\apk\\TrianguloApp.apk";
+    public static final String APK_LOCATION_M = System.getProperty(USER_DIR) + "/src/test/resources/apk/TrianguloApp.apk";
 
-   File f = new File("\\src\\main\\resources\\apk\\TrianguloApp.apk");
+    public static String platformType = ANDROID;
 
+    //TODO: Fix capabilities iOS and run parallel test
     public static void capabilities() throws MalformedURLException {
-        final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
         url = new URL(URL_STRING);
         capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 3a API 28");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"9.0");
-        capabilities.setCapability(MobileCapabilityType.APP,"\\APK\\TrianguloApp.apk");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.CLEAR_SYSTEM_FILES,true);
-        capabilities.setCapability(MobileCapabilityType.NO_RESET,true);
-        capabilities.setCapability("waitForQuiescence", false);
-        capabilities.setCapability("useNewWDA",false);
-        driver = new AndroidDriver(url, capabilities);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        if (platformType.equalsIgnoreCase(ANDROID)) {
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, ANDROID_DEVICE);
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, ANDROID_VERSION);
+            if (SO.contains(WINDOWS)) capabilities.setCapability(MobileCapabilityType.APP, APK_LOCATION_W); else capabilities.setCapability(MobileCapabilityType.APP, APK_LOCATION_M);
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, ANDROID_AUTOMATION);
+            capabilities.setCapability(MobileCapabilityType.CLEAR_SYSTEM_FILES, true);
+            androidDriver = new AndroidDriver(url, capabilities);
+            androidDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        } else if (platformType.equalsIgnoreCase(IOS)) {
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, iOS_DEVICE);
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, iOS_VERSION);
+            if (SO.contains(WINDOWS)) capabilities.setCapability(MobileCapabilityType.APP, APK_LOCATION_W); else capabilities.setCapability(MobileCapabilityType.APP, APK_LOCATION_M);
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, ANDROID_AUTOMATION);
+            capabilities.setCapability(MobileCapabilityType.CLEAR_SYSTEM_FILES, true);
+            iosDriver = new IOSDriver(url, capabilities);
+            iosDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        }
     }
 }
---
+++ src/test/java/utils/Resources.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Resources {
+
+    //Capabilities
+    public static final String ANDROID = "Android";
+    public static final String WINDOWS = "Windows";
+    public static final String USER_DIR = "user.dir";
+    public static final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
+    public static final String ANDROID_DEVICE = "Pixel 3a API 28";
+    public static final String ANDROID_VERSION = "9.0";
+    public static final String ANDROID_AUTOMATION = "UiAutomator2";
+
+    //iOS
+    public static final String IOS = "iOS";
+    public static final String iOS_DEVICE = "Pixel 3a API 28";
+    public static final String iOS_VERSION = "9.0";
+    public static final String iOS_AUTOMATION = "UiAutomator2";
+}
